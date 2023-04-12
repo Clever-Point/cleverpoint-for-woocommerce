e9cbbf94ef0b73793597b00ecfc056b48c696b0f
Web Expert <info@webexpert.gr>
2023-04-12 09:22:56 +0300

    1.0.5

--- clever-point-for-woocommerce.php
+++ clever-point-for-woocommerce.php
@@ -46,7 +46,7 @@ if (get_option('clever_point_test_mode',null)=='yes') {
 
 require plugin_dir_path(__FILE__) . 'includes/update/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/Clever-Point/woocommerce-plugin',
+    'https://github.com/Clever-Point/clever-point-for-woocommerce',
     __FILE__,
     'clever-point-for-woocommerce'
 );
